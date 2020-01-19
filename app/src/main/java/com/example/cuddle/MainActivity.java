3f0cb4658225c17e01d1a75615937614344c2ffb
AndrewShakula <43982937+Drewsovsky@users.noreply.github.com>
2020-01-19 19:22:44 +0200

    Update MainActivity.java
    Added funcrion to register matches with both users.

--- app/src/main/java/com/example/cuddle/MainActivity.java
+++ app/src/main/java/com/example/cuddle/MainActivity.java
@@ -20,6 +20,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
@@ -91,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
                         .child("Yes")
                         .child(currentUid).setValue(true);
 
+                isConnectionMach(userId);
+
                 Toast.makeText(MainActivity.this, "Right!", Toast.LENGTH_SHORT).show();
             }
 
@@ -114,6 +117,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void isConnectionMach(String userId) {
+        DatabaseReference currentUserConnectionsDb = usersDb.child(userSex)
+                .child(currentUid)
+                .child("Connections")
+                .child("Yes")
+                .child(userId);
+        currentUserConnectionsDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(MainActivity.this, "New Connection", Toast.LENGTH_LONG).show();
+                    usersDb.child(oppositeUserSex)
+                            .child(dataSnapshot.getKey())
+                            .child("Connections")
+                            .child("Matches")
+                            .child(currentUid).setValue(true);
+                    usersDb.child(userSex)
+                            .child(currentUid)
+                            .child("Connections")
+                            .child("Matches")
+                            .child(dataSnapshot.getKey()).setValue(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private String userSex;
